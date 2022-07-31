7a4f5aaa7b1b9c1d451539c8ace85122d5921f97
dan-k-park <dan.k.park@gmail.com>
2022-07-31 03:23:20 -1000

    navbar is now responsive

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,15 +1,35 @@
-import React, { useEffect } from "react";
+import {
+  Chat,
+  Notifications,
+  Person,
+  Search,
+  Cancel,
+} from "@mui/icons-material";
+import { useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
-import { Chat, Person, Search, Notifications } from "@mui/icons-material";
+
 export const Navbar = () => {
   const publicFolder = process.env.PUBLIC_URL;
   const user = {
     username: "blurper",
   };
+  const [searchOpen, setSearchOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState({ width: 0 });
 
   useEffect(() => {
-    console.log(process.env.REACT_APP_PUBLIC_FOLDER);
-  }, []);
+    const handleResize = () => {
+      setWindowWidth({
+        width: window.innerWidth,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    if (windowWidth.width > 640) {
+      setSearchOpen(false);
+    }
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, [windowWidth.width]);
 
   return (
     <div className="h-[50px] w-full flex items-center sticky top-0 z-10 bg-sky-500">
@@ -20,28 +40,48 @@ export const Navbar = () => {
           </span>
         </Link>
       </div>
-      <div className="flex-[6]">
-        <div className="flex items-center w-full h-[30px] bg-white rounded-[30px]">
-          <Search className="text-[20px] ml-[10px]" />
-          <input className="border-none w-[70%] focus:outline-none" />
+      <div className="flex flex-[8] sm:flex-[6]">
+        <div
+          className={`${
+            searchOpen ? "flex" : "hidden"
+          } sm:flex items-center w-[90%] sm:w-full h-[30px] bg-white rounded-[30px]`}
+        >
+          <Search className="text-[18px] sm:text-[20px] ml-[5px] sm:ml-[10px]" />
+          <input className="border-none w-full sm:w-[70%] focus:outline-none" />
+          <Cancel
+            className="text-[18px] sm:hidden mr-[5px] cursor-pointer"
+            onClick={() => setSearchOpen(false)}
+          />
         </div>
       </div>
-      <div className="flex-[4] flex items-center justify-end mr-[10px] text-white">
+      <div
+        className={`${
+          searchOpen ? "hidden" : "flex-[4]"
+        } flex-[4] flex items-center justify-end mr-[10px] text-white}`}
+      >
         <div className="flex">
-          <div className="mr-[15px] cursor-poitner relative">
-            <Person />
-            <span className="absolute top-[-5px] rounded-full right-[-5px] flex items-center justify-center w-[15px] h-[15px] bg-red-500 text-[12px]">
+          <div
+            className={`sm:hidden ${
+              searchOpen ? "hidden" : "block"
+            } mr-[15px] cursor-pointer relative`}
+            onClick={() => setSearchOpen(true)}
+          >
+            <Search className="text-white" />
+          </div>
+          <div className="mr-[15px] cursor-pointer relative">
+            <Person className="text-white" />
+            <span className="absolute top-[-5px] rounded-full right-[-5px] flex items-center justify-center w-[15px] h-[15px] bg-red-500 text-white  text-[12px]">
               2
             </span>
           </div>
-          <div className="mr-[15px] cursor-poitner relative">
-            <Chat />
+          <div className="mr-[15px] cursor-pointer relative">
+            <Chat className="text-white" />
             <span className="absolute top-[-5px] rounded-full right-[-5px] flex items-center justify-center w-[15px] h-[15px] bg-red-500 text-white text-[12px]">
               2
             </span>
           </div>
-          <div className="mr-[15px] cursor-poitner relative">
-            <Notifications />
+          <div className="mr-[15px] cursor-pointer relative">
+            <Notifications className="text-white" />
             <span className="absolute top-[-5px] rounded-full right-[-5px] flex items-center justify-center w-[15px] h-[15px] bg-red-500 text-white text-[12px]">
               2
             </span>
